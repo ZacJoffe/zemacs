e08129cb5d2a679e505c53e9b567bfcbf6963261
Zac Joffe <zacharyjoffe@gmail.com>
2022-02-17 22:52:34 -0500

    Fix straight bootstrap

--- init.el
+++ init.el
@@ -262,6 +262,7 @@ With argument, do this that many times."
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
