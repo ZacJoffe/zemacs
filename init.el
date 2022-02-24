551c2f4e88bce25fabdfd313d5428077103b1658
Zac Joffe <zacharyjoffe@gmail.com>
2022-02-24 00:05:45 -0500

    Corfu basic config

--- init.el
+++ init.el
@@ -1413,6 +1413,9 @@ _j_   zoom-out
     "z=" 'flyspell-correct-wrapper
     "C-;" 'flyspell-correct-wrapper
 
+    ;; movement
+    "C-n" 'evil-next-visual-line ;; TODO should be in motion? doesn't seem to go down to these states?
+    "C-p" 'evil-previous-visual-line
     "s" 'avy-goto-char)
 
   ;; insert mode hotkeys
@@ -1659,6 +1662,72 @@ _j_   zoom-out
   :config
   (company-flx-mode +1))
 
+;; TODO try out corfu
+(use-package corfu
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; You may want to enable Corfu only for certain modes.
+  :hook (prog-mode . corfu-mode)
+  :general
+  (:keymaps 'corfu-map
+            "C-n" 'corfu-next
+            "C-p" 'corfu-previous
+            "C-SPC" 'corfu-insert-separator)
+
+  :init
+  (corfu-global-mode)
+  :config
+  ;; HACK evil keymaps seem to take precedence over corfu's map, use advice to fix
+  ;; https://github.com/minad/corfu/issues/12#issuecomment-881961510
+  (advice-add 'corfu--setup :after 'evil-normalize-keymaps)
+  (advice-add 'corfu--teardown :after 'evil-normalize-keymaps)
+  (evil-make-overriding-map corfu-map))
+
+;(use-package corfu-doc
+;  :after corfu
+;  :hook (corfu-mode . corfu-doc-mode))
+
+;; Add extensions
+(use-package cape
+  ;; Bind dedicated completion commands
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-symbol)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p i" . cape-ispell)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
+  :init
+  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+)
 ;;----
 
 
