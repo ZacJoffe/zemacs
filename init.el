49cb328d88e637651ba992f942701d786f1ed852
Zac Joffe <zacharyjoffe@gmail.com>
2022-08-11 18:15:16 -0400

    Consult flyspell

--- init.el
+++ init.el
@@ -642,6 +642,16 @@
 (use-package consult-flycheck
   :after (consult flycheck))
 
+;; integration with flyspell
+(use-package consult-flyspell
+  :straight (consult-flyspell :type git :host gitlab :repo "OlMon/consult-flyspell" :branch "master")
+  :config
+  ;; default settings
+  (setq consult-flyspell-select-function nil
+        consult-flyspell-set-point-after-word t
+        consult-flyspell-always-check-buffer nil))
+
+
 ;; consult functions
 (defun +consult/find-file (DIR)
   "Open file in directory DIR."
